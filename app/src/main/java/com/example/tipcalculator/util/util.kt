3bd3512dd91a889e4ef99d9315225bc0aba1d560
Manjunath R K <rkmanjunath10000@gmail.com>
2022-10-02 19:05:18 +0530

    Add starter code for util.kt

---
+++ app/src/main/java/com/example/tipcalculator/util/util.kt
@@ -0,0 +1,11 @@
+package com.example.tipcalculator.util
+
+fun calculateTotalTip(totalBill: Double, tipPercentage: Int): Double
+{
+
+}
+
+fun calculateTotalPerson(totalBill: Double, splitBy: Int, tipPercentage: Int ): Double
+{
+
+}
